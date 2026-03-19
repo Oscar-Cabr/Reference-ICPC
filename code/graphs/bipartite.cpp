30fc4390f59a7a2a53cd7843ace3bb045b07baed
Oscar-Cabr <oscar.cabrerarojas@gmail.com>
2026-03-19 00:19:41 -0600

    added: checking bipartiteness

---
+++ code/graphs/bipartite.cpp
@@ -0,0 +1,24 @@
+vi cnt(3,0), color(V+1,0);
+function<void(int)> bfs = [&](int s) -> void {
+  queue<int> q;
+  bool bipartite = true;
+  q.push(s);
+  color[s] = 1;
+  cnt[1]++;
+  while( ! q.empty() ) {
+    int u = q.front();
+    q.pop();
+    for(int v : adj[u]) {
+      if( color[v] == 0 ) {
+        color[v] = 3 - color[u];
+        cnt[ color[v] ]++;
+        q.push(v);
+      }
+      else if( color[v] == color[u] )
+        bipartite = false;
+    }
+  }
+  if(!bipartite) {
+    cnt[1] = cnt[2] = false;
+  }
+};
