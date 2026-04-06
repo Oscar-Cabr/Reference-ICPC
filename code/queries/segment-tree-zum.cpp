992bd100ba09873237cb4dde4797e27317ef2499
zums-stuff <jafet.hernandez.ed@gmail.com>
2026-04-06 11:36:46 -0600

    Updated: code/queries/segment-tree-zum.cpp

--- code/queries/segment-tree-zum.cpp
+++ code/queries/segment-tree-zum.cpp
@@ -1,33 +1,56 @@
-vector<long long> v, sex;
-int n;
-void build(int node, int l, int r){
-    if(l == r) sex[node] = v[l];
-    else{
-        int mid = (l+r)/2;
-        build(2*node, l, mid);
-        build(2*node + 1, mid+1, r);
-        sex[node] = sex[2*node] + sex[2*node +1];
+#include<bits/stdc++.h>
+typedef long long ll;
+struct Node{
+    ll zum;
+    Node() : zum(0) {}
+
+    Node(ll val){
+        zum = val;
     }
-}
-void update(int node, int l, int r, int idx, int val){
-    if(l == r){
-        v[idx] = val;
-        sex[node] = val;
+
+    static Node merge(const Node& a, const Node& b){
+        Node res;
+        res.zum = a.zum + b.zum;
+        return res;
     }
-    else{
-        int mid = (l+r)/2;
-        if(l <= idx && idx <= mid) update(2*node, l, mid, idx, val);
-        else update(2*node +1, mid+1, r, idx, val);
-        sex[node] = sex[2*node] + sex[2*node + 1];
+
+};
+
+struct SegmentTree{
+    int n;
+    vector<Node> tree;
+
+    SegmentTree(int n) : n(n), tree(4*n){}
+
+    void build(const vector<ll>& a, int node, int l, int r){
+        if(l==r){
+            tree[node] = Node(a[l]);
+            return;
+        }
+        int mid = l + (r-l)/2;
+        build(a, 2*node, l, mid);
+        build(a, 2*node + 1, mid + 1, r);
+        tree[node] = Node::merge(tree[2*node], tree[2*node + 1]);
     }
-}
-int query(int node, int tl, int tr, int l, int r){
-    if(r < tl || tr < l) return 0;
-    if(l <= tl && tr <= r) return sex[node];
-    int tm = (tl+tr)/2;
-    return query(2*node, tl, tm, l, r) + query(2*node +1, tm+1, tr, l, r);
-}
-v.resize(n);
-sex.resize(4 * n);
-build(1, 0, n - 1);
-query(1, 0, n-1, l - 1, r - 1)
+
+    void update(int node, int l, int r, int idx, ll x){
+        if(l == r){
+            tree[node] = Node(x);
+            return;
+        }
+        int mid = l + (r-l)/2;
+        if(idx <= mid) update(2*node, l, mid, idx, x);
+        else update(2*node + 1, mid + 1, r, idx, x);
+        tree[node] = Node::merge(tree[2*node], tree[2*node + 1]);
+    }
+
+    Node query(int node, int l, int r, int tl, int tr){
+        if(tl > r || tr < l) return Node();
+        if(tl <= l && r <= tr) return tree[node];
+        int mid = l + (r-l)/2;
+        return Node::merge(
+            query(2*node, l, mid, tl, tr),
+            query(2*node + 1, mid + 1, r, tl, tr)
+        );
+    }
+};
